406e0b3925eeb56f4a52f71c7ebcf2d0602e289e
msong793y <miles.song.ca@gmail.com>
2020-04-19 14:53:52 -0700

    canvas added, webpack hooked up

---
+++ src/index.js
@@ -0,0 +1,25 @@
+console.log("Webpack is working!");
+
+// const Game = require("./game.js");
+// const GameView = require("./game_view.js");
+// const Modal = require("./modal");
+
+// const MovingObject = require("./moving_object.js");
+
+// testingg
+// window.MovingObject = MovingObject;
+
+// 
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    console.log("DomLoaded")
+
+
+//   const canvas = document.getElementById("game-canvas");
+//   const ctx = canvas.getContext("2d");
+
+//   const view = new GameView(ctx, canvas, game);
+ 
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname),
+        filename: "bundle.js"
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: ["*", ".js"]
+    }
+};
